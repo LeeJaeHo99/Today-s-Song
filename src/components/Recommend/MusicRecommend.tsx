1f179037b3dff3da339a4495410430508740edb9
LeeJaeHo99 <ljh2735294@naver.com>
2025-01-29 23:59:01 +0900

    recommend section 완

---
+++ src/components/Recommend/MusicRecommend.tsx
@@ -0,0 +1,32 @@
+import Image from 'next/image';
+
+export default function MusicRecommend() {
+    return (
+        <>
+            <form className="music-recommend--component" action="">
+                <div>
+                    <label htmlFor="recommend-title">
+                        <span>제목</span>
+                        <input
+                            id="recommend-title"
+                            type="text"
+                            placeholder="노래의 제목을 입력해주세요."
+                        />
+                    </label>
+                    <label htmlFor="recommend-singer">
+                        <span>가수</span>
+                        <input
+                            id="recommend-singer"
+                            type="text"
+                            placeholder="노래의 가수를 입력해주세요."
+                        />
+                    </label>
+                </div>
+            </form>
+            <button className="music-recommend-submit--btn" type="button">
+                <span>제출하기</span>
+                <Image src={'/icons/arrow.png'} width={20} height={20} alt='arrow-icon'/>
+            </button>
+        </>
+    );
+}
--- src/pages/main/MusicRecommendSection.tsx
+++ src/pages/main/MusicRecommendSection.tsx
@@ -1,5 +1,6 @@
 import SectionTitle from "@/components/SectionTitle";
 import SubTitle from "@/components/SubTitle";
+import MusicRecommend from '@/components/Recommend/MusicRecommend';
 
 export default function MusicRecommendSection() {
     return (
@@ -7,6 +8,7 @@ export default function MusicRecommendSection() {
             <div className="inner">
                 <SectionTitle title={`노래 신청하기`} />
                 <SubTitle title={`자신의 플레이리스트에 있는 최애곡을 추천해 주세요.`}/>
+                <MusicRecommend/>
             </div>
         </section>
     );
