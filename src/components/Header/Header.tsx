d1737210cf0523c57171bf05aec5f46b587bc486
LeeJaeHo99 <ljh2735294@naver.com>
2025-01-30 18:26:12 +0900

    header 스크롤 이벤트

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,28 @@
+'use client';
+
 import Link from 'next/link';
+import { useEffect, useState } from 'react';
 
 export default function Header() {
+    const [isScrolled, setIsScrolled] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if(window.scrollY >= 200){
+                setIsScrolled(true);
+            }else{
+                setIsScrolled(false);
+            }
+        }
+        window.addEventListener('scroll', handleScroll);
+        
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        }
+    })
+
     return (
-        <header>
+        <header className={`${isScrolled && 'scrolled'}`}>
             <div className="inner">
                 <h1>
                     💿 오느래 노래
@@ -18,6 +38,9 @@ function HeaderList(){
         <ul>
             <li>
                 <Link href={'/'}>공지사항</Link>
+            </li>
+            <li>
+                <Link href={'/'}>감상평</Link>
             </li>
             <li>
                 <Link href={'/'}>노래신청</Link>
