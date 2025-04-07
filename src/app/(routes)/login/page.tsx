c6b9937b865682ddc1c07037777ddd0fe5b881b2
LeeJaeHo99 <ljh2735294@naver.com>
2025-04-07 15:45:22 +0900

    로그인 페이지 제작

--- src/app/(routes)/login/page.tsx
+++ src/app/(routes)/login/page.tsx
@@ -1,7 +1,13 @@
+import LoginComponent from '@/components/login/LoginComponent';
+import SectionTitle from '@/components/SectionTitle';
+
 export default function AuthPage(){
     return(
         <div className="auth-page sub-page--wrap">
-
+            <div className="inner">
+                <SectionTitle title={"로그인"} />
+                <LoginComponent/>
+            </div>
         </div>
     );
 }
