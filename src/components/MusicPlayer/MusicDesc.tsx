11d5df3678311c6235c2dbbec13b4e17aa531cf2
LeeJaeHo99 <ljh2735294@naver.com>
2025-06-17 13:52:28 +0900

    Music Desc ISR로 변경

--- src/components/MusicPlayer/MusicDesc.tsx
+++ src/components/MusicPlayer/MusicDesc.tsx
@@ -1,43 +1,25 @@
-"use client";
+export const revalidate = 60 * 60 * 6;
 
-import { useEffect, useState } from "react";
+export default async function MusicDesc() {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/getMusic`, {
+        next: { revalidate: revalidate },
+    });
+    const result = await res.json();
 
-export default function MusicDesc() {
-    const [title, setTitle] = useState<string>("로딩 중...");
-    const [singer, setSinger] = useState<string>("");
+    const now = new Date();
+    const hours = now.getHours();
+    const isMorning = hours > 6 && hours < 18 ? "morning" : "night";
 
-    useEffect(() => {
-        const now = new Date();
-        const hours = now.getHours();
-        const isMorning = hours > 6 && hours < 18 ? "morning" : "night";
-
-        const fetchData = async () => {
-            try {
-                const response = await fetch(
-                    `${process.env.NEXT_PUBLIC_API_URL}/api/getMusic`
-                );
-                const result = await response.json();
-                const data = result.data[result.data.length - 1][isMorning];
-                setTitle(data.title);
-                setSinger(data.singer);
-            } catch (error) {
-                setTitle("데이터를 불러오는 중 오류가 발생했습니다.");
-                setSinger("");
-                console.error(error);
-            }
-        };
-
-        fetchData();
-    }, []);
+    const data = result.data[result.data.length - 1][isMorning];
 
     return (
         <div className="music-desc--wrap">
             <ul className="music-desc">
                 <li>
-                    <div className="title view">{title}</div>
+                    <div className="title view">{data.title}</div>
                 </li>
                 <li>
-                    <div className="singer view">{singer}</div>
+                    <div className="singer view">{data.singer}</div>
                 </li>
             </ul>
         </div>
