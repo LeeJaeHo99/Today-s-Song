c6e4e3b71c2306e4df7f455b6a96e6ab840256d6
LeeJaeHo99 <ljh2735294@naver.com>
2025-06-19 18:17:55 +0900

    콘솔제거 + ISR 코드 변경

--- src/components/MusicPlayer/MusicDesc.tsx
+++ src/components/MusicPlayer/MusicDesc.tsx
@@ -1,14 +1,22 @@
 export const revalidate = 60 * 60 * 6;
 
 export default async function MusicDesc() {
+    const now = new Date();
+    const hours = now.getHours();
+    
+    const dateStr = now.toISOString().split('T')[0];
+    const timeSlot = hours < 18 && hours >= 6 ? 'morning' : 'night';
+    const cacheKey = `${dateStr}-${timeSlot}`;
+
     const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/getMusic`, {
-        next: { revalidate: revalidate },
+        next: { 
+            tags: [cacheKey],
+            revalidate: 0
+        },
     });
     const result = await res.json();
 
-    const now = new Date();
-    const hours = now.getHours();
-    const isMorning = hours > 6 && hours < 18 ? "morning" : "night";
+    const isMorning = hours >= 6 && hours < 18 ? "morning" : "night";
     const data = result.data[result.data.length - 1][isMorning];
 
     return (
